b24e67c0cad899374e195c917fa9ee68a797f67f
Rainer Loopere <rainer.loopere@gmail.com>
2020-03-14 16:14:57 +0100

    Initial commit for Spring set-up

---
+++ TestRegister/src/main/java/com/lavantru/TestRegister/TestRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.lavantru.TestRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRegisterApplication.class, args);
+	}
+
+}
---
+++ TestRegister/src/test/java/com/lavantru/TestRegister/TestRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavantru.TestRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
