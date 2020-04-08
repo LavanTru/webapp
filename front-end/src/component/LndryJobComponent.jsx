7abbad5a8c03510937a244a5eb24ae6d909048ae
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-04-08 14:28:34 +0200

    Create initial LndryJobDetailsComponent class

--- front-end/src/component/LndryJobComponent.jsx
+++ front-end/src/component/LndryJobComponent.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import ListLndryJobsComponent from './ListLndryJobsComponent';
+import LndryJobDetailsComponent from './LndryJobDetailsComponent';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
 
 class LndryJobComponent extends Component {
     render() {
         return (
-            <>
-              <h1>Laundry Job</h1>
-              <ListLndryJobsComponent/>
-            </>
+            <Router>
+                <>
+                    <h1>Laundry Job</h1>
+                    <Switch>
+                        <Route path="/" exact component={ListLndryJobsComponent} />
+                        <Route path="/laundryJobs" exact component={ListLndryJobsComponent} />
+                        <Route path="/laundryJobs/:id" component={LndryJobDetailsComponent} />
+                    </Switch>
+                </>
+            </Router>
         )
     }
 }
---
+++ front-end/src/component/LndryJobDetailsComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component }  from 'react';
+
+class LndryJobDetailsComponent extends Component {
+    render() {
+      return (
+            <h1>Laundry Job Details</h1>
+      )
+  }
+
+}
+
+export default LndryJobDetailsComponent
