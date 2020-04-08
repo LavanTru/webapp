3c3f1853421209d33777b30b285092fffc83192f
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-04-08 13:50:49 +0200

    Fix updateJobById method in LndryJobController

--- Register/src/main/java/com/lavantru/Register/dao/LndryJobDataAccessService.java
+++ Register/src/main/java/com/lavantru/Register/dao/LndryJobDataAccessService.java
@@ -20,47 +20,39 @@ public class LndryJobDataAccessService implements LndryJobDao {
     @Override
     public int insertJob(UUID id, LndryJob lndryJob) {
         repository.save(new LndryJob(id, lndryJob.getJob()));
-        //DB.add(new LndryJob(id, lndryJob.getJob()));
         return 1;
     }
 
     @Override
     public List<LndryJob> selectAllJobs() {
-        //return DB;
         return repository.findAll();
     }
 
     @Override
     public Optional<LndryJob> selectJobById(UUID id) {
         return repository.findById(id);
-//        return DB.stream()
-//                .filter(lndryJob -> lndryJob.getId().equals(id))
-//                .findFirst();
     }
 
     @Override
     public int deleteJobById(UUID id) {
         repository.deleteById(id);
         return 1;
-//        Optional<LndryJob> lndryJob = selectJobById(id);
-//        if(lndryJob.isPresent()){
-//            DB.remove(lndryJob.get());
-//            return 1;
-//        }
-//        return 0;
     }
 
     @Override
     public int updateJobById(UUID id, LndryJob lndryJob) {
-        return selectJobById(id)
-                .map(lj -> {
-                    int indexOfJobToUpdate = DB.indexOf(lj);
-                    if (indexOfJobToUpdate >= 0){
-                        DB.set(indexOfJobToUpdate, new LndryJob(id, lndryJob.getJob()));
-                        return 1;
-                    }
-                    return 0;
-                })
-                .orElse(0);
+        lndryJob.setId(id);
+        repository.save(lndryJob);
+        return 1;
+//        return selectJobById(id)
+//                .map(lj -> {
+//                    int indexOfJobToUpdate = DB.indexOf(lj);
+//                    if (indexOfJobToUpdate >= 0){
+//                        DB.set(indexOfJobToUpdate, new LndryJob(id, lndryJob.getJob()));
+//                        return 1;
+//                    }
+//                    return 0;
+//                })
+//                .orElse(0);
     }
 }
