5f7ad56720fa6c1d75f261c393fbb8b2b033b810
Rainer Loopere <rainer.loopere@gmail.com>
2020-04-26 17:49:07 +0200

    Fixed imports

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,6 @@ import WasherJobCapabilitiesComponent from './component/WasherJobCapabilitiesCom
 import RegisterWasherOrWashee from './component/RegisterWasherOrWashee';
 import RegisterWasher from './component/RegisterWasher';
 import { SessionContext, getSessionCookie } from "./Session";
-import WasherJobCapabilitiesComponent from './component/WasherJobCapabilitiesComponent';
 
 
 /**React Component representing the high-level structure of the application. 
