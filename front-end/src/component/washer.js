30d644a541205d573640822570d78dc5a1928acd
paulc <paulc@slixa.com>
2020-04-29 21:01:38 +0200

    initial skeleton

--- front-end/src/component/washer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const washer = (props) => {
-    return( <li>
-        <span>name: {props.children}, adresses= {props.name}</span>
-    </li>)
-}
-
-export default washer;
