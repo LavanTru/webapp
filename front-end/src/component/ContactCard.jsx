1f364b6b798a1a2ec44be5207f3531e437ea202b
Rainer Loopere <rainer.loopere@gmail.com>
2020-05-01 17:30:21 +0200

    Finished profile page design

--- front-end/src/component/ContactCard.jsx
+++ front-end/src/component/ContactCard.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import {Card} from "react-bootstrap";
+import { Card,Button } from "react-bootstrap";
 
 class ContactCard extends Component {
-constructor (props) {
-    super(props);
-    this.state = {
-        firstName: "Gertrud"
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: "Gertrud",
+            streetName: "Monec",
+            buildingNo: "16",
+            apartmentNo: "3-1",
+            postCode: "08003",
+            city: "Barcelona",
+            phoneNo: "+34654253847",
+        }
     }
-}
 
     render() {
         return (
             <Card>
-                <Card.Header>Contact {this.state.firstName}</Card.Header>
-                
+                <Card.Header className="h2">Contact {this.state.firstName}</Card.Header>
+                <Card.Body>
+                <Card.Title>Address</Card.Title>
+                <Card.Text>{this.state.streetName}, {this.state.buildingNo} {this.state.apartmentNo}
+                    <br /> {this.state.city} {this.state.postCode}</Card.Text>
+                <Card.Title>Phone number</Card.Title>
+                <Card.Text> {this.state.phoneNo}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                <Button className="button-green" block>Set up wash</Button>
+                </Card.Footer>
+
             </Card>
         );
     }
--- front-end/src/component/WasherProfile.jsx
+++ front-end/src/component/WasherProfile.jsx
@@ -1,21 +1,85 @@
 import React, { Component } from 'react';
-import {Container,Col,Row,Card} from "react-bootstrap";
+import { Container, Col, Row, Card } from "react-bootstrap";
 import ContactCard from "./ContactCard";
+import WasherDataService from '../service/WasherDataService';
 
 class WasherProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            washerId: '5ea1d1552cea5c7ddc865ded', // id must be consumed from the user session, 
+            firstName: "Gertrud",
+            aboutMe: "I don't like untidy environment and household. I work has a quality and very high presentation.I take pride in my work and love cleaning and tidying leaving the environment spotless, fragrant and looking like brand new restored place. The level of my work is very professional and high quality in line with required standards and specific ways of doing things. I have an experience of many years.          Many thanks and best wishes and looking forward to hear from you.",
+            washerCapabilities: []
+        }
+    }
+
+    componentDidMount() {
+        this.refreshWasherJobCapabilities();
+    }
+
     render() {
         return (
-            <Container>
-                <Row>
+            <Container className="profile">
                 <Col>
-                <ContactCard/>
+                    <Row className="mt-5">
+                        <Col md={8}>
+                            <div >
+                                <div className="header">
+                                    <img className="image float-left" src="https://image.shutterstock.com/image-photo/cook-pleasure-waist-cheerful-senior-260nw-757384063.jpg" alt="" />
+                                    <div className="text-center my-5">
+                                        <h1>{this.state.firstName}</h1>
+                                        <p>10 washes </p>
+                                    </div>
+                                </div>
+                                <p>
+                                    <h2 className="mt-3">About me</h2>
+                                    <p>{this.state.aboutMe}</p>
+                                    <h2 className="mt-3">My services</h2>
+                                    <Row>
+                                                    <Col>
+                                                    </Col>
+                                                    <Col>
+                                                    <b>Speed</b>
+                                                    </Col>
+                                                </Row>
+                                
+                                <div className="container">
+                                    {
+                                        this.state.washerCapabilities.map(
+                                            washerJob =>
+                                                <Row>
+                                                    <Col>
+                                                        <h5>{(washerJob.active) ?  (washerJob.job):""}</h5>
+                                                    </Col>
+                                                    <Col>
+                                                    {(washerJob.active) ? (washerJob.speed) : ''}
+                                                    </Col>
+                                                </Row>
+                                        )
+                                    }
+                                </div>
+
+                                </p>
+                            </div>
+                        </Col>
+                        <Col >
+                            <ContactCard />
+                        </Col>
+                    </Row>
                 </Col>
-                <Col>
-                <ContactCard/>
-                </Col>
-                </Row>
             </Container>
         );
+    }
+
+    refreshWasherJobCapabilities() {
+        WasherDataService.retrieveWasher(this.state.washerId)
+            .then(
+                response => {
+                    this.setState({ washerCapabilities: response.data.jobCapabilities })
+                    console.log(this.state.washerCapabilities)
+                }
+            )
     }
 }
 
