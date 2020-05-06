1518ece06beb00315434e55b8505439f7e89c6b5
Rainer Loopere <rainer.loopere@gmail.com>
2020-05-06 11:49:54 +0200

    Added a placeholder card element

--- front-end/src/component/WasherRequestConfirm.jsx
+++ front-end/src/component/WasherRequestConfirm.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { Container, Col, Row, Card,Button } from "react-bootstrap";
 
 class WasherRequestConfirm extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Container>
+                <Col>
+                    <Card>
+                        <Card.Header> Request placeholder</Card.Header>
+                        <Card.Body>
+                            <Card.Text>Here goes some request information</Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button className="button-green" onClick={this.handleConfirmButton}>Confirm</Button>
+                            <Button className="button-pink" onClick={this.handleRejectButton} >Reject</Button>
+                        </Card.Footer>
+                    </Card>
+                </Col>
+            </Container>
         );
+    }
+
+    handleConfirmButton(){
+
+    }
+
+    handleRejectButton(){
+        
     }
 }
 
