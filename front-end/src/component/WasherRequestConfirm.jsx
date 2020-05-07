3f758aa968ad3f9e34d0fd5516e413221beb8d35
Rainer Loopere <rainer.loopere@gmail.com>
2020-05-07 19:54:04 +0200

    Added call to backend to get order

--- front-end/src/component/WasherRequestConfirm.jsx
+++ front-end/src/component/WasherRequestConfirm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Col, Row, Card,Button } from "react-bootstrap";
+import { Container, Col, Row, Card, Button } from "react-bootstrap";
 import OrderDataService from "../service/OrderDataService"
 
 class WasherRequestConfirm extends Component {
@@ -12,6 +12,11 @@ class WasherRequestConfirm extends Component {
         this.handleRejectButton = this.handleRejectButton.bind(this);
     }
 
+
+    componentDidMount() {
+        this.refreshOrderDetails();
+    }
+
     render() {
         return (
             <Container>
@@ -22,20 +27,49 @@ class WasherRequestConfirm extends Component {
                             <Card.Text>Here goes some request information</Card.Text>
                         </Card.Body>
                         <Card.Footer>
-                            <Button className="button-green" onClick={this.handleConfirmButton}>Confirm</Button>
-                            <Button className="button-pink" onClick={this.handleRejectButton} >Reject</Button>
+                            {this.renderFooter()}
                         </Card.Footer>
                     </Card>
                 </Col>
             </Container>
         );
     }
+    renderFooter() {
+        if (this.state.status !== "New") {
+            return <div>Status: <b>{this.state.status}</b></div>;
+        }
+        else {
+            return (
+                <div>
+                    <Button className="button-green" onClick={this.handleConfirmButton}>Confirm</Button>
+                    <Button className="button-pink" onClick={this.handleRejectButton} >Reject</Button>
+                </div>
+            )
+        }
+    }
 
-    handleConfirmButton(){
+    refreshOrderDetails() {
+        OrderDataService.getOrderById(this.state.orderId)
+            .then(
+                response => {
+                    this.setState({
+                        status: response.data.status,
+                        notes: response.data.notes,
+                        dateCreated: response.data.dateCreated,
+                        washeeId: response.data.washeeId,
+                        items: response.data.items
+                    })
+                }
+            ).then(
+                () => { console.log("new state", this.state) }
+            )
+    }
+
+    handleConfirmButton() {
         OrderDataService.confirmOrder(this.state.orderId);
     }
 
-    handleRejectButton(){
+    handleRejectButton() {
         OrderDataService.rejectOrder(this.state.orderId);
     }
 }
