1226ac62d5b0356e10b36fa06d6210ced2a846cd
Rainer Loopere <rainer.loopere@gmail.com>
2020-05-26 19:38:03 +0200

    Fixed background highlighting bug

--- front-end/src/component/WasherSelection/WasherList/WasherList.jsx
+++ front-end/src/component/WasherSelection/WasherList/WasherList.jsx
@@ -4,14 +4,15 @@ import WasherListItem from './WasherListItem';
 
 class WasherList extends Component {
     render() {
-
+        //If any marker exists, we get the washerId. Otherwise app will fail trying to access it.
+        const activeWasherId = (this.props.activeMarker)? this.props.activeMarker.washerId : "";
         return (
             <div className="washerList">
                 {
                     this.props.washers.map(
                         washer =>
                             // If the Washer is selected in the WasherSelectionContainer view then pink background is used
-                            <WasherListItem key={washer.id} washer={washer} backgroundColour={(washer.id === (this.props.activeMarker.washerId&&true)) ? "pinkBackground" : ""} />
+                            <WasherListItem key={washer.id} washer={washer} backgroundColour={(washer.id === activeWasherId) ? "pinkBackground" : ""} />
                     )
                 }
             </div>
