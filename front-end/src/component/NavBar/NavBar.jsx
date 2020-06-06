52b8857839c97cb67e273df6027bc02b832c734b
Rainer Loopere <rainer.loopere@gmail.com>
2020-06-06 13:00:12 +0200

    Showing number of notifications on navbar

--- front-end/src/component/NavBar/NavBar.jsx
+++ front-end/src/component/NavBar/NavBar.jsx
@@ -2,17 +2,24 @@ import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import ModalContainer from "../Modal/ModalContainer";
 import NavBarLoginInfo from "./NavBarLoginInfo";
+import OrderDataService from "../../service/OrderDataService";
+import { SessionContext } from "../../Session";
 
 class NavBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             loginModalShow: false,
-            isRegistered: true
+            isRegistered: true,
+            newOrders: ""
         };
         this.handleLoginModal = this.handleLoginModal.bind(this);
         this.handleChangeIsRegistered = this.handleChangeIsRegistered.bind(this);
+        this.refreshOrderList = this.refreshOrderList.bind(this);
+    }
 
+    componentDidMount() {
+        this.refreshOrderList();
     }
 
     handleLoginModal() {
@@ -23,28 +30,38 @@ class NavBar extends Component {
         this.setState({ isRegistered: !this.state.isRegistered });
     };
 
+    refreshOrderList() {
+        if (this.context && this.context.userType === "WASHER") {
+            OrderDataService.getOrdersByWasherId(this.context.id)
+                .then(
+                    response => {
+                        this.setState({
+                            newOrders: (response.data.filter(order => order.status === "NEW").length === 0 ? "" : response.data.filter(order => order.status === "NEW").length)
+                        })
+                    }
+                )
+        }
+    }
 
     render() {
         return (
-            <Navbar className="navbar" expand="lg">
+            <Navbar className="navbar" expand="lg" >
                 <Navbar.Brand href="#home">LavanTru</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/washers">Washers</Nav.Link>
-                        <Nav.Link href="/washerOrderList">Orders</Nav.Link>
+                        <Nav.Link href="/washerOrderList">Orders{this.state.newOrders && < span className="notifications"> {this.state.newOrders} </span>}</Nav.Link>
                     </Nav>
                     <Nav className="ml-auto">
-                        <NavBarLoginInfo handleChangeIsRegistered={this.handleChangeIsRegistered} handleLoginModal={this.handleLoginModal}/>
+                        <NavBarLoginInfo handleChangeIsRegistered={this.handleChangeIsRegistered} handleLoginModal={this.handleLoginModal} />
                         <ModalContainer
                             show={this.state.loginModalShow}
-                            // show={true}
                             onHide={this.handleLoginModal}
                             isRegistered={this.state.isRegistered}
                             handleChangeIsRegistered={this.handleChangeIsRegistered}
                         />
-                        {this.myMethod}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
@@ -52,5 +69,6 @@ class NavBar extends Component {
     }
 
 }
+NavBar.contextType = SessionContext;
 
 export default NavBar;
