3a3687e6d94f2ff5e7a0784080d59707dd234019
Rainer Loopere <rainer.loopere@gmail.com>
2020-06-06 14:06:00 +0200

    Added badges to dashboard

--- front-end/src/component/Dashboard/WasherDashboard.jsx
+++ front-end/src/component/Dashboard/WasherDashboard.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Col, Row, Card, Button, Accordion, Alert } from "react-bootstrap";
+import { Container, Col, Row, Card } from "react-bootstrap";
 import { Bar } from 'react-chartjs-2';
+import BadgeCompletedWash from "../../asset/icon/badge_completed_wash.svg";
+import BadgeTopWasher from "../../asset/icon/badge_top_washer.svg";
 
 class WasherDashboard extends Component {
 
@@ -70,7 +72,7 @@ class WasherDashboard extends Component {
                                 <Card.Header className="header">Completed washes</Card.Header>
                                 <Card.Body>
                                     <img className="image float-left" src="https://image.flaticon.com/icons/svg/2101/2101382.svg" alt="total_washes" />
-                                    <h2>18</h2>
+                                    <h2>61</h2>
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -84,30 +86,43 @@ class WasherDashboard extends Component {
                             </Card>
                         </Col>
                     </Row>
-                    <Row className="mt-5">
-                        <h4>Monthly earnings</h4>
-                        <Bar
-                            height={100}
-                            data={data}
-                            options={options}
-                        />
-                    </Row>
-                    <Row className="my-5">
+                    <Row className="mt-3">
                         <Col>
-                            <Card className="clickable">
-                                <Card.Header className="header">Service settings</Card.Header>
-                                <Card.Body>
-                                    <img className="image float-center" src="https://image.flaticon.com/icons/svg/3003/3003800.svg" alt="service_settings" />
-                                </Card.Body>
-                            </Card>
+                            <h4>Monthly earnings</h4>
+                            <Bar
+                                height={100}
+                                data={data}
+                                options={options}
+                            />
+                        </Col>
+                        <Col md={4}>
+                            <Row className="justify-content-md-center">
+                                <Card className="clickable">
+                                    <Card.Header className="header">Service settings</Card.Header>
+                                    <Card.Body>
+                                        <img className="image float-center" src="https://image.flaticon.com/icons/svg/3003/3003800.svg" alt="service_settings" />
+                                    </Card.Body>
+                                </Card>
+                            </Row>
+                            <Row className="mt-3 justify-content-md-center">
+                                <Card className="clickable">
+                                    <Card.Header id="profile" className="header">Profile info</Card.Header>
+                                    <Card.Body>
+                                        <img className="image float-center" src="https://cdn0.iconfinder.com/data/icons/basic-ui-1-line/64/Artboard_18-512.png" alt="profile_info" />
+                                    </Card.Body>
+                                </Card>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row className="mt-3"><h4>Badges</h4></Row>
+                    <Row>
+                        <Col>
+                            <img className="" src={BadgeCompletedWash} alt="Completed wash badge" />
+                            <h6>Completed 50 washes</h6>
                         </Col>
                         <Col>
-                            <Card className="clickable">
-                                <Card.Header className="header">Profile info</Card.Header>
-                                <Card.Body>
-                                    <img className="image float-center" src="https://cdn0.iconfinder.com/data/icons/basic-ui-1-line/64/Artboard_18-512.png" alt="profile_info" />
-                                </Card.Body>
-                            </Card>
+                            <img className="" src={BadgeTopWasher} alt="Top washer badge" />
+                            <h6>Best rated washer</h6>
                         </Col>
                     </Row>
                 </Col>
