4ce9e0b9bb071e619edf67e6d77c230953400fdc
Rainer Loopere <rainer.loopere@gmail.com>
2020-06-10 19:08:07 +0200

    Added filtering and sorting buttons

--- front-end/src/component/WasherSelection/WasherList/WasherList.jsx
+++ front-end/src/component/WasherSelection/WasherList/WasherList.jsx
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
 import WasherListItem from './WasherListItem';
 import GeoLibService from "../../../service/GeoLibService";
+import { OverlayTrigger, Popover, Form, Col, Row } from "react-bootstrap";
 
 
 class WasherList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            sortByClosest:true
+        }
+        this.handleSortClick = this.handleSortClick.bind(this);
+    }
+
+    
+    handleSortClick () {
+        this.setState({
+            sortByClosest: !this.state.sortByClosest
+        })
+    }
+
     render() {
-        console.log("washers", this.props.washers);
         //If any marker exists, we get the washerId. Otherwise app will fail trying to access it.
         const activeWasherId = (this.props.activeMarker) ? this.props.activeMarker.washerId : "";
+
+        const popover = (
+            <Popover id="popover-basic">
+                <Popover.Content>
+                    <Form.Check className="m-2" type="radio" label="Ironing" />
+                    <Form.Check className="m-2" type="radio" label="Laundry" />
+                    <Form.Check className="m-2" type="radio" label="Dry cleaning" />
+                </Popover.Content>
+            </Popover>
+        );
+
         return (
             <div className="washerList">
+                
+                <Row className="mb-3">
+                    <h4>Filters:</h4>
+                    <Col><OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                        <div className="filter">Services</div>
+                    </OverlayTrigger></Col>
+                    <Col md="auto">
+                        <Row>
+                            {/* Conditional formatting based on which sorting option is active */}
+                            <div onClick={this.handleSortClick} className={"clickable sort-left " + (this.state.sortByClosest? "sort-active":"")}>Closest first</div>
+                            <div onClick={this.handleSortClick} className={"clickable sort-right " + (!this.state.sortByClosest? "sort-active":"")}> Cheapest first</div>
+                            </Row>
+                    </Col>
+                </Row>
+
                 {
                     this.props.washers.map(
                         washer =>
