32fd527508a2686a00ea5beaec9883ba9351f67e
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-06-13 20:25:28 +0200

    Move Components to folder

---
+++ front-end/src/component/IconsComponents/TemperatureIcons.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import TemperatureIcon from './TemperatureIcon';
+
+function TemperatureIcons(props){
+    switch (props.temperature){
+        case 30:
+            return(
+                <>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#b8627d" 
+                        temp="90"
+                        >
+                    </TemperatureIcon>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#b8627d" 
+                        temp="60">
+                    </TemperatureIcon>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#0f7d80" 
+                        temp={props.temperature}>
+                    </TemperatureIcon>
+                </>
+            );
+        case 60:
+            return(
+                <>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#b8627d" 
+                        temp="90">
+                    </TemperatureIcon>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#0f7d80"
+                        temp={props.temperature}>    
+                    </TemperatureIcon>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#b8627d" 
+                        temp="30">
+                    </TemperatureIcon>
+                </>
+            );
+        case 90:
+            return(
+                <>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#0f7d80"
+                        temp={props.temperature}>
+                    </TemperatureIcon>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#b8627d" 
+                        temp="60">
+                    </TemperatureIcon>
+                    <TemperatureIcon 
+                        className="wash-icon"
+                        fill="#b8627d" 
+                        temp="30">
+                    </TemperatureIcon>
+                </>
+            );
+        default: 
+        return(<><h4>Invalid temperature</h4></>)
+    }
+
+}
+
+export default TemperatureIcons
