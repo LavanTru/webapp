783d120ee8725f64cfa755858d9a831a7dfefbfc
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-06-14 20:30:13 +0200

    Modify OrderListItems with order attributes

--- front-end/src/component/Order/WasherOrderListItem.jsx
+++ front-end/src/component/Order/WasherOrderListItem.jsx
@@ -7,14 +7,24 @@ const WasherOrderListItem = (props) => {
             <Col xs sm="auto" className="p-0"><img className="image float-left" src={props.order.washeeImage} alt="profile_picture" /></Col>
             <Col >
                 <Row> <h3 className="text-center">Order from {props.order.washeeFirstName}</h3> </Row>
-                <h6>Services:</h6>
-                {props.order.items.map(
-                    item =>
-                        <div>{item.job}</div>
-                )}
+                <h6>{props.order.deliveryByWashee ? "Washee comes to you"  : "You go to washee"}</h6>
+                <Row>
+                    <Col>
+                    <p><b>Services requested:</b>{props.order.items.map(
+                        item =>
+                            <div>{item.job}</div>
+                    )}</p>
+                    
+                    </Col>
+                    <Col>
+                    <p>Give laundry: {props.order.pickup}</p>
+                    <p>Receive laundry: {props.order.dropoff}</p>
+                    </Col>
+                </Row>
+                
             </Col>
             <Col className="text-center my-5" md="auto">
-                <h1 >€ 30</h1>
+                <h1 >€ {props.order.orderTotal}</h1>
                 <b>{(props.order.status === "NEW") ? "" : props.order.status}</b>
             </Col>
         </Row>
